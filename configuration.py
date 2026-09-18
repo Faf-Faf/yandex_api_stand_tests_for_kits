2869e4be21bf8dc80c6916e03f424ef585d8c4f7
Liliya <welma20038@gmail.com>
2026-09-19 02:18:15 +0500

    first commit in here

--- configuration.py
+++ configuration.py
@@ -1,7 +1,3 @@
 URL_SERVICE = "https://e819870e-19f1-4afb-ae9d-6abea9d33765.serverhub.praktikum-services.ru"
 
-CREATE_USER_PATH = "/api/v1/users/" 
-
 CREATE_KIT_PATH = "/api/v1/kits/"
-
-PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
--- sender_stand_request.py
+++ sender_stand_request.py
@@ -4,11 +4,6 @@ import requests
 
 import data 
 
-def post_new_user(body):
-    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
-                         json=body,
-                         headers=data.headers)
-
 def post_new_kit(body):
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_KIT_PATH,
                          json=body,
